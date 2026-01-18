95f9cd85369d643bad57138fcaee7a183044b71e
Ethan Dagnone <ethandagnone@gmail.com>
2026-01-18 15:29:04 -0500

    Improve resize reliability

--- follow.py
+++ follow.py
@@ -12,29 +12,25 @@ HYPERDASH_PATH = "C:/Program Files/Oculus/Software/Software/triangle-factory-hyp
 pyautogui.PAUSE = 0.5
 pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR/tesseract.exe'
 
-def window_is_fullscreen(window):
-    #This is a bad way of checking, but the last way broke with multi-monitors
-    return window.left == 0 and window.top == 0    
-
-def wait_for_hyperdash_window():
+def wait_for_hyperdash_window()-> gw.Win32Window:
     print("Waiting for HD window to appear")
     while True:
         for w in gw.getWindowsWithTitle("Hyper Dash"):
             print("FOUND", w)
+            w.activate()
             return w
         time.sleep(0.1)
 
-def resize_hyperdash_window(window, width, height):
-    window.moveTo(3,3)
-    if window_is_fullscreen(window):
-        print(f"Exiting fullscreen")
+def resize_hyperdash_window(window: gw.Win32Window, width: int, height: int):
+    while window.size.width != width or window.size.height != height:
         pyautogui.hotkey('Alt', 'Enter')
-    print(f"Resizing to {width}x{height}")
-    window.resizeTo(width, height)
-    window.moveTo(3,3)
+        time.sleep(0.1)
+        print(f"Resizing to {width}x{height}")
+        window.resizeTo(width, height)
+        print("Size is now", window.size)
     return window
     
-def wait_for_black_alternation(x_offset, y_offset):
+def wait_for_black_alternation(x_offset: int, y_offset: int):
     print("Waiting for game to load")
     while (True):
         if(pyautogui.pixelMatchesColor(150+ x_offset, 150 + y_offset, (0,0,0))):
@@ -45,7 +41,7 @@ def wait_for_black_alternation(x_offset, y_offset):
                 time.sleep(0.1)
         time.sleep(0.1)
 
-def get_player_index(slot_regions, player_to_follow):
+def get_player_index(region:list[tuple[int,  int, int, int]], player_to_follow: str):
     print("Capturing Images")
     for _ in range(5): #Retry 5x if needed
         for i, region in enumerate(slot_regions, start=1):
@@ -118,10 +114,11 @@ print("Joining", server['name'])
 pyautogui.hotkey('Alt', 'Enter') # Un-Fullscreen
 time.sleep(0.1)
 window = resize_hyperdash_window(window, 800, 600)
+time.sleep(0.1)
 pyautogui.press('b')
 time.sleep(0.1)
 pyautogui.click(729 + x, 264 + y)
-time.sleep(0.2)
+time.sleep(0.5)
 pyautogui.press('b')
 
 pyautogui.click(500 + x, 325 + y) #Server Browser
